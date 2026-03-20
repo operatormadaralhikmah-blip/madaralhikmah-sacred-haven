c2439b51f3d6a41622ef94a8067951f5739148c5
operatormadaralhikmah-blip <operator.madaralhikmah@gmail.com>
2026-03-20 11:24:30 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/madaralhikmah-sacred-haven/",
   plugins: [react()],
   resolve: {
     alias: {
